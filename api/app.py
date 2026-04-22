38c5c4a0d3a583f0f23b0a91a7e77983ba38832b
AnmolGanorkar <anmolganorkar@gmail.com>
2026-04-22 17:31:03 +0530

    final api fix

--- api/app.py
+++ api/app.py
@@ -1,21 +1,36 @@
-# API application
-# TODO: Implement Flask/FastAPI app
-
-import os
-from dotenv import load_dotenv
 from fastapi import FastAPI
 import sqlite3
+import os
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+from database.db import init_db, insert_data
 
 load_dotenv()
 
 app = FastAPI()
 
+# CORS FIRST
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 DB_NAME = os.getenv("DB_PATH", "data/products.db")
+
+# 🔥 IMPORTANT FOR RENDER
+@app.on_event("startup")
+def startup():
+    init_db()
+    insert_data()
+
 
 @app.get("/")
 def home():
     return {"message": "Price Tracker API Running"}
+
 
 @app.get("/products")
 def get_products():
@@ -26,18 +41,9 @@ def get_products():
     data = cursor.fetchall()
 
     conn.close()
-
     return {"data": data}
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-# ✅ NEW ENDPOINT
 @app.get("/average-price")
 def average_price():
     conn = sqlite3.connect(DB_NAME)
